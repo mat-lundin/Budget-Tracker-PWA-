ad0b910facf89a9bae36c4208c4e2f1ad1435c49
Mat Lundin <matmll12@gmail.com>
2021-11-28 16:04:43 -0600

    added mongo db server connection

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGO_DB_UI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
